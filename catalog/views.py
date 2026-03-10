9a98382d61796b209790c463a16ba6dda8c06d35
Anastasia <goncharovaanastasia53@gmail.com>
2026-03-10 23:54:28 +0700

    Creating a home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    num_instances_avialable=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()
+
+    count_genres=Genre.objects.filter(name__iregex=r'\b' + 'фантастика' + r'\b').count()
+    count_books=Book.objects.filter(title__iregex=r'\b' + 'window' + r'\b').count()
+
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_avialable, 'num_authors':num_authors,
+                 'count_genres':count_genres, 'count_book':count_books},
+    )
