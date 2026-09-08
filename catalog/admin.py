75e4498750367ac28baf8296fb71cde406b983d7
goncharovaav <goncharovaav@users.noreply.github.com>
2026-09-08 19:38:23 +0700

    Update project: restructure templates, add migrations, fix settings and auth templates per MDN tutorial

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,38 +1,16 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
-# admin.site.register(Book)
-# admin.site.register(Author)
 admin.site.register(Genre)
-# admin.site.register(BookInstance)
 
 class BooksInline(admin.TabularInline):
     model = Book
 
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
-
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
-
-admin.site.register(Author, AuthorAdmin)
-
-
-@admin.register(BookInstance)
-class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
-    list_filter = ('status', 'due_back')
-
-    fieldsets = (
-    (None, {
-        'fields': ('book', 'imprint', 'id')
-    }),
-    ('Availability', {
-        'fields': ('status', 'due_back', 'borrower')
-    }),
-    )
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
@@ -41,3 +19,17 @@ class BooksInstanceInline(admin.TabularInline):
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
